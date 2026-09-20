0e4cb7b1710e17d350f5a56b23cf5b5d3c32aebe
nikshep <klimenko.leva.lol2@yandex.ru>
2026-09-20 18:26:15 +0800

    add abstract reference class

---
+++ Src/Core/Abstract.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from uuid import uuid4
+
+
+class BaseEntity(ABC):
+    """Базовый класс для доменных сущностей."""
+
+    def __init__(self):
+        """Инициализирует базовую сущность."""
+        self.__id = str(uuid4())
+        self.__name = ""
+
+    @property
+    def id(self) -> str:
+        """Возвращает уникальный идентификатор сущности."""
+        return self.__id
+
+    @property
+    def name(self) -> str:
+        """Возвращает наименование сущности."""
+
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        """Изменяет наименование сущности."""
+        if not value or not value.strip():
+            raise ValueError("Имя не может быть пустым")
+
+        self.__name = value.strip()
